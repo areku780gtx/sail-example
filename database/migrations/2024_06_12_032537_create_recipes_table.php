d4e659512a2fe584381189e456429bbc92189ce2
areku780gtx <arekux800@gmail.com>
2024-06-12 14:38:21 +0900

    maigration uuid 設定

--- database/migrations/2024_06_12_032537_create_recipes_table.php
+++ database/migrations/2024_06_12_032537_create_recipes_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->uuid('id',36)->primary();
+            $table->foreignUuId('user_id')->constrained()->onDelete('cascade');
+            
+
+
+
+
+
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('recipes');
     }
 };
